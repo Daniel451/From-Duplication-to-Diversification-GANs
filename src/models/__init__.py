4a8a4d5cf02995b2d2b87388868f4976ee519131
Daniel Speck <daniel451@mailbox.org>
2023-12-11 15:43:19 +0100

    import fixes

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 from ._discriminator import Discriminator
 from ._generator import Generator
-from .model_dcgan import DCGenerator, DCDiscriminator
+from .model_dcgan import Discriminator as DCGANDiscriminator
+from .model_dcgan import Generator as DCGANGenerator
